25a469f669c6df44c39ccd73811f10f4571979ca
roman.nikitin44 <rnikitin@mmtr.ru>
2024-10-08 19:59:26 +0300

    50-weather-components Решение задачи

--- 03-components/50-weather-components/WeatherDetailsItem.js
+++ 03-components/50-weather-components/WeatherDetailsItem.js
@@ -18,7 +18,7 @@ export default defineComponent({
   template: `
       <div class="weather-details__item">
         <div class="weather-details__item-label">{{ label }}</div>
-        <div class="weather-details__item-weather">{{ value }}</div>
+        <div class="weather-details__item-value">{{ value }}</div>
       </div>
   `,
 })
